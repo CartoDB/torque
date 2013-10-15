a75aa736d0750fb69a7ea2123fd157a57fc69e43
javi <qualopec@gmail.com>
2013-10-15 10:40:31 +0200

    added extra params

--- test/provider.json.js
+++ test/provider.json.js
@@ -1,13 +1,25 @@
 
-module('provider.json');
-
-  test("url", function() {
-    var json = new torque.providers.json({
+var json, url;
+module('provider.json')
+QUnit.testStart(function() {
+    json = new torque.providers.json({
       user: "rambo",
       resolution: 1,
-      steps: 10
+      steps: 10,
+      extra_params: {
+        testing: 'abcd%'
+      }
     });
+});
+
+  test("url", function() {
     equal("http://rambo.cartodb.com/api/v2/sql", json.url());
+  });
+
+  test("extra_params", function() {
+    var url = "http://rambo.cartodb.com/api/v2/sql?q=1&testing=abcd%25";
+    json.sql('1');
+    equal(torque.net.lastCall().url, url);
   });
 
 
