6e3703b980c3042dbff541d9a95650aed8c32c91
javi <qualopec@gmail.com>
2013-10-17 15:51:20 +0200

    missing test

---
+++ test/core.js
@@ -0,0 +1,27 @@
+module('torque.core.Event');
+
+  function TestObj(){}
+  TestObj.prototype = torque.Event;
+
+  test("on", function() {
+    var called = false;
+    var o = new TestObj();
+    o.on('test', function()  { called = true});
+    o.trigger('test');
+    equal(called, true);
+  });
+
+  test("off", function() {
+    var called = false;
+    var o = new TestObj();
+    function fn()  { called = true; }
+    o.on('test', fn); 
+    equal(o.callbacks('test').length, 1);
+    o.off('test', fn);
+    equal(o.callbacks('test').length, 0);
+    o.trigger('test');
+    equal(called, false);
+    o.on('test', fn); 
+    o.off('test');
+    equal(o.callbacks('test').length, 0);
+  });
