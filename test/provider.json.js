cf23d6c5b22e933e23a9800da9cf4d9f025e407f
javi <qualopec@gmail.com>
2013-10-17 15:51:12 +0200

    multiple fixes

--- test/provider.json.js
+++ test/provider.json.js
@@ -3,6 +3,7 @@ var json, url;
 module('provider.json')
 QUnit.testStart(function() {
     json = new torque.providers.json({
+      table: 'test',
       user: "rambo",
       resolution: 1,
       steps: 10,
@@ -20,6 +21,15 @@ QUnit.testStart(function() {
     var url = "http://rambo.cartodb.com/api/v2/sql?q=1&testing=abcd%25";
     json.sql('1');
     equal(torque.net.lastCall().url, url);
+  });
+
+  test("getSQL", function() {
+    var s;
+    equal(json.getSQL(), "select * from test");
+    json.setSQL(s='select * from test limit 10');
+    equal(json.getSQL(), s);
+    json.setSQL(null);
+    equal(json.getSQL(), "select * from test");
   });
 
 
