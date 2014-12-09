e8fee1659bcf11e03129acc419cc741bdc0b83d2
Raul Ochoa <rochoaf@gmail.com>
2014-12-09 18:06:56 +0100

    Fix suite, use `make prepare-test-suite` and open test/suite.html or run `npm test`.

---
+++ test/suite.js
@@ -0,0 +1,9 @@
+require('qunitjs');
+
+require('./core');
+require('./torque');
+require('./renderer/point');
+require('./provider.jsonarray');
+require('./provider.windshaft.test');
+require('./provider.json');
+require('./request');
