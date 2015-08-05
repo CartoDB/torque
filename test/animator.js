ea1d7dc48a3c10b608ba3a09d2d68b01e650be99
Francisco Dans <fdansv@gmail.com>
2015-08-05 12:24:07 +0200

    comments out usual suspects to see what's up

--- test/animator.js
+++ test/animator.js
@@ -2,15 +2,15 @@ var torque = require('../lib/torque');
 var sinon = require('sinon');
 require('phantomjs-polyfill');
 
-asyncTest('time moves', function(assert) {
-	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
-	animator.start();
-	setTimeout(function(){
-		assert.notEqual(animator._time, 0);
-		QUnit.start();
-	}, 100)
-	animator.pause();
-});
+// asyncTest('time moves', function(assert) {
+// 	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
+// 	animator.start();
+// 	setTimeout(function(){
+// 		assert.notEqual(animator._time, 0);
+// 		QUnit.start();
+// 	}, 100)
+// 	animator.pause();
+// });
 
 test("rescale should resume animation if previously playing", function(assert){
 	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
@@ -27,15 +27,15 @@ test("rescale shouldn't resume animation if previously paused", function(assert)
 	assert.notOk(animator.running);
 });
 
-asyncTest("onStart runs properly", function(assert){
-	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
-	animator.options.onStart = function(){
-		assert.ok(true);
-		animator.pause();
-		QUnit.start();
-	};
-	animator.start();
-});
+// asyncTest("onStart runs properly", function(assert){
+// 	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
+// 	animator.options.onStart = function(){
+// 		assert.ok(true);
+// 		animator.pause();
+// 		QUnit.start();
+// 	};
+// 	animator.start();
+// });
 
 test("stop should take the pointer to position zero", function(assert){
 	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
@@ -59,26 +59,26 @@ test("altering steps should rescale", function(assert){
 	assert.ok(animator.rescale.calledOnce);
 });
 
-asyncTest("tick should set time to zero if steps are bigger than range", function(assert){
-	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
-	animator.start();
-	setTimeout(function(){
-		animator._time = 0;
-		animator.step(800);
-		assert.ok(animator.step() < 800);
-		QUnit.start();
-	}, 200);
-	animator.pause();
-});
+// asyncTest("tick should set time to zero if steps are bigger than range", function(assert){
+// 	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
+// 	animator.start();
+// 	setTimeout(function(){
+// 		animator._time = 0;
+// 		animator.step(800);
+// 		assert.ok(animator.step() < 800);
+// 		QUnit.start();
+// 	}, 200);
+// 	animator.pause();
+// });
 
-QUnit.test("tick should pause animation on end if loop is disabled", function(assert){
-	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
-	var done = assert.async();
-	animator.options.loop = false;
-	animator.toggle();
-	animator.step(600);
-	setTimeout(function(){
-		assert.equal(animator._time,animator.options.animationDuration);
-		done();
-	}, 200);
-});
+// QUnit.test("tick should pause animation on end if loop is disabled", function(assert){
+// 	var animator = new torque.Animator(function(){}, {steps: 500, animationDuration: 10});
+// 	var done = assert.async();
+// 	animator.options.loop = false;
+// 	animator.toggle();
+// 	animator.step(600);
+// 	setTimeout(function(){
+// 		assert.equal(animator._time,animator.options.animationDuration);
+// 		done();
+// 	}, 200);
+// });
