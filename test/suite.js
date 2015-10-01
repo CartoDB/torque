3099b7b881f355b33bde03ae930925b1a1888325
Francisco Dans <fdansv@gmail.com>
2015-10-01 18:42:42 +0200

    adds tilejson to suite

--- test/suite.js
+++ test/suite.js
@@ -4,6 +4,7 @@ require('./core');
 require('./torque');
 require('./renderer/point');
 require('./provider.jsonarray');
+require('./provider.tilejson');
 require('./provider.windshaft.test');
 require('./provider.json');
 require('./request');
