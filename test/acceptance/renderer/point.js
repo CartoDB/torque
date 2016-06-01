5bdabbd921b679edebaf68beef30b0d030557d24
Raul Ochoa <rochoaf@gmail.com>
2016-06-02 01:42:45 +0200

    Add support for different tile sizes

--- test/acceptance/renderer/point.js
+++ test/acceptance/renderer/point.js
@@ -10,37 +10,53 @@ var IMAGE_DIFF_TOLERANCE = 4 / 100;
 // Once you have a valid canvas and no errors, it's possible to write to disk the canvas buffer as a png image with:
 // require('fs').writeFileSync('/tmp/torque-acceptance-test-tile.png', canvas.toBuffer(), {encoding: null});
 
-asyncTest('navy example', function(assert) {
-    var cartocss = [
-        'Map {',
-        '  -torque-time-attribute: "date";',
-        '  -torque-aggregation-function: "count(cartodb_id)";',
-        '  -torque-frame-count: 760;',
-        '  -torque-animation-duration: 15;',
-        '  -torque-resolution: 2',
-        '}',
-        '#layer {',
-        '  marker-width: 3;',
-        '  marker-fill-opacity: 0.8;',
-        '  marker-fill: #FEE391; ',
-        '  comp-op: "lighten";',
-        '  [value > 2] { marker-fill: #FEC44F; }',
-        '  [value > 3] { marker-fill: #FE9929; }',
-        '  [value > 4] { marker-fill: #EC7014; }',
-        '  [value > 5] { marker-fill: #CC4C02; }',
-        '  [value > 6] { marker-fill: #993404; }',
-        '  [value > 7] { marker-fill: #662506; }',
-        '  [frame-offset = 1] { marker-width: 10; marker-fill-opacity: 0.05;}',
-        '  [frame-offset = 2] { marker-width: 15; marker-fill-opacity: 0.02;}',
-        '}'
-    ].join('\n');
+var cartocss = [
+    'Map {',
+    '  -torque-time-attribute: "date";',
+    '  -torque-aggregation-function: "count(cartodb_id)";',
+    '  -torque-frame-count: 760;',
+    '  -torque-animation-duration: 15;',
+    '  -torque-resolution: 2',
+    '}',
+    '#layer {',
+    '  marker-width: 3;',
+    '  marker-fill-opacity: 0.8;',
+    '  marker-fill: #FEE391; ',
+    '  comp-op: "lighten";',
+    '  [value > 2] { marker-fill: #FEC44F; }',
+    '  [value > 3] { marker-fill: #FE9929; }',
+    '  [value > 4] { marker-fill: #EC7014; }',
+    '  [value > 5] { marker-fill: #CC4C02; }',
+    '  [value > 6] { marker-fill: #993404; }',
+    '  [value > 7] { marker-fill: #662506; }',
+    '  [frame-offset = 1] { marker-width: 10; marker-fill-opacity: 0.05;}',
+    '  [frame-offset = 2] { marker-width: 15; marker-fill-opacity: 0.02;}',
+    '}'
+].join('\n');
 
+asyncTest('navy example', function(assert) {
     var step = 300;
 
     pointRenderer.getTile('default_navy_3-3-2.torque.json', cartocss, 3, 3, 2, step, function(err, canvas) {
         assert.ok(!err, 'no error while getting tile');
         var imageDiff = image.compare(canvas.toBuffer(), 'default_navy_3-3-2.png');
         assert.ok(imageDiff < IMAGE_DIFF_TOLERANCE, 'navy tile is ok');
+        QUnit.start();
+    });
+});
+
+asyncTest('tileSize = 512', function(assert) {
+    var step = 300;
+    var tileSize = 512;
+    var options = {
+        tileSize: tileSize
+    };
+
+    pointRenderer.getTile('default_navy_3-3-2.torque.json', cartocss, 3, 3, 2, step, options, function(err, canvas) {
+        assert.ok(!err, 'no error while getting tile');
+        var img = image.getImage(canvas.toBuffer());
+        assert.equal(img.width(), tileSize);
+        assert.equal(img.height(), tileSize);
         QUnit.start();
     });
 });
